19bec5417c06b835c1470208e39ce6df3e054844
Avram86 <60349668+Avram86@users.noreply.github.com>
2021-07-15 19:18:02 +0300

    Add project files.

---
+++ OOPInterfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOPInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
